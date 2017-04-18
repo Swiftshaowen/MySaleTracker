53c23b3779e563f3a686f5c5a975855ce669e463
weijie <weijie.wang@tinno.com>
2017-04-18 16:16:17 +0800

    ApeSaleTracker_V7.0.10.13--modify wifi send

--- src/main/java/com/ape/saletracker/SaleTrackerService.java
+++ src/main/java/com/ape/saletracker/SaleTrackerService.java
@@ -734,7 +734,7 @@ public class SaleTrackerService extends Service {
 
 		// 只考虑连接数据网络的情况
 		if (mobile.getState() == NetworkInfo.State.CONNECTED
-				|| mobile.getState() == NetworkInfo.State.CONNECTED) {  //getState()方法是查询是否连接了数据网络
+				|| wifi.getState() == NetworkInfo.State.CONNECTED) {  //getState()方法是查询是否连接了数据网络
 			return true;
 		}
 		return false;
